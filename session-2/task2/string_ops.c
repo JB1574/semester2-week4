ca6b04987be1b7dff8cd6394a1b8127aff6878f0
JB1574 <mrln0367@leeds.ac.uk>
2026-02-20 10:05:57 +0000

    end of task 2

--- session-2/task2/string_ops.c
+++ session-2/task2/string_ops.c
@@ -12,12 +12,19 @@ int main( void ) {
     char str3[100];
 
     // use printf to verify the following tests
-
+    printf("As array: %s\n", str1);
+    printf("As array: %s\n", str2);
+    printf("As array: %s\n", str3);
     // use 'strcmp' to compare 2 strings - print the return value
-
+    strcmp(str1,str2);
+    int result = strcmp(str1,str2);
+    printf("return value %d\n ", result);
     // use 'strcat' to concatenate 2 strings - print the resulting string
-
+    strcat(str1,str2);
+    printf("new string: %s\n", str1);
     // use 'strcpy' to copy a string into str3 - print string 3
+    strcpy(str3,str1);
+    printf("copied string: %s \n", str3);
 
     return 0;
 }
